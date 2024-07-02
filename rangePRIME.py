45488a1f7aef88ca1b9c6b5c52a6c30612e07ede
Aniket Mane <111334212+aniketmane18@users.noreply.github.com>
2024-07-02 18:12:30 +0530

    by Aniket Mane

---
+++ rangePRIME.py
@@ -0,0 +1,13 @@
+n = int(input("Enter Number :"))
+k = 2
+while k <= n:
+    d = 2
+    flag = False
+    while d < k:
+        if k % d == 0:
+            flag = True
+            break
+        d = d + 1
+    if not(flag):
+        print(k)
+    k = k+1
